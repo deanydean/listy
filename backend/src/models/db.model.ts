42e721b4395f3b5a7c3431bf332b8b3bbfc42741
Zak Catherall <zak.catherall@outlook.com>
2022-12-22 14:30:06 +0000

    Create interface for DB shared functions, plus mongodb and test implementation.

--- backend/src/models/db.model.ts
+++ backend/src/models/db.model.ts
@@ -1,21 +1,22 @@
-import * as mongo from 'mongodb'
+import * as mongo from 'mongodb';
 
-import { Response } from './response.model'
+import { Response } from './response.model';
 
+// TODO: getRecordById().
+// TODO: getAllRecords() should be paginated, or an additional paginated get should be created.
 export interface Db {
-  readonly _db: MockDb | mongo.Db
-  getRecordById: <T>(table: string, id: string) => Promise<Response<T>>
+  readonly _db: MockDb | mongo.Db;
   editRecordById: <T extends mongo.Document>(
     table: string,
     id: string,
     object: T
-  ) => Promise<Response<string>>
-  deleteRecordById: (table: string, id: string) => Promise<Response<string>>
+  ) => Promise<Response<string>>;
+  deleteRecordById: (table: string, id: string) => Promise<Response<string>>;
   createRecord: <T extends mongo.Document>(
     table: string,
     object: T
-  ) => Promise<Response<string>>
-  getAllRecords: <T>(table: string) => Promise<Response<T[]>>
+  ) => Promise<Response<string>>;
+  getAllRecords: <T>(table: string) => Promise<Response<T[]>>;
 }
 
-export type MockDb = null
+export type MockDb = null;
--- backend/src/shared/testdb.ts
+++ backend/src/shared/testdb.ts
@@ -1,40 +1,36 @@
-import * as mongo from 'mongodb'
+import * as mongo from 'mongodb';
 
-import { Db, MockDb } from '../models/db.model'
+import { Db, MockDb } from '../models/db.model';
 
-import { Response } from '../models/response.model'
+import { Response } from '../models/response.model';
 
 export class TestDb implements Db {
-  readonly _db: MockDb
+  readonly _db: MockDb;
 
-  constructor () {
-    this._db = null
+  constructor() {
+    this._db = null;
   }
 
-  async getRecordById<T>(table: string, id: string): Promise<Response<T>> {
-    return new Response({ status: 200, data: { table, id } })
-  }
-
-  async editRecordById (
+  async editRecordById(
     table: string,
     id: string,
     changes: Object
   ): Promise<Response<string>> {
-    return new Response({ status: 200, data: { table, id, changes } })
+    return new Response({ status: 200, data: { table, id, changes } });
   }
 
-  async deleteRecordById (table: string, id: string): Promise<Response<string>> {
-    return new Response({ status: 200, data: { table, id } })
+  async deleteRecordById(table: string, id: string): Promise<Response<string>> {
+    return new Response({ status: 200, data: { table, id } });
   }
 
   async createRecord<T extends mongo.Document>(
     table: string,
     object: T
   ): Promise<Response<string>> {
-    return new Response({ status: 200, data: { table, object } })
+    return new Response({ status: 200, data: { table, object } });
   }
 
   async getAllRecords<T>(table: string): Promise<Response<T[]>> {
-    return new Response({ status: 200, data: { table } })
+    return new Response({ status: 200, data: { table } });
   }
 }
