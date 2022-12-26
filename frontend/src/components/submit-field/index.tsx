37dab5f951ce1ff63b2560561f1124740517b8c1
Zak Catherall <zak.catherall@outlook.com>
2022-12-26 14:58:42 +0000

    Create component for submitting text.

---
+++ frontend/src/components/submit-field/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export interface SubmitFieldProps {
+  placeholder: string;
+  maxLength: number;
+  submitHandler: Function;
+}
+
+const SubmitField = ({
+  placeholder,
+  maxLength,
+  submitHandler,
+}: SubmitFieldProps): JSX.Element => {
+  const [text, setText] = useState<string>('');
+
+  const userPressedEnter = (e: React.KeyboardEvent<HTMLElement>): boolean => {
+    return e.key === 'Enter';
+  };
+
+  const handleSubmit = (): void => {
+    if (text.length > 0) {
+      submitHandler();
+      setText('');
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      maxLength={maxLength}
+      onChange={(e) => setText(e.target.value)}
+      onKeyDown={(e) => userPressedEnter(e) && handleSubmit()}
+      value={text}
+    />
+  );
+};
+
+export default SubmitField;
