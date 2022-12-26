98ca6e98a1d7b25a497f07b6c1c788e4a9abd179
Zak Catherall <zak.catherall@outlook.com>
2022-12-26 15:03:44 +0000

    Create list item component.

--- frontend/src/components/list/list-item/index.tsx
+++ frontend/src/components/list/list-item/index.tsx
@@ -1,8 +1,39 @@
-import { ListItem as ListItemModel } from '../../../models/list';
 import React from 'react';
+import styled from 'styled-components';
 
-const ListItem = (listItem: ListItemModel): JSX.Element => (
-  <div>{listItem.text}</div>
-);
+export interface ListItemProps {
+  text: string;
+  completed: boolean;
+  completedHandler: Function;
+  deleteHandler: Function;
+}
+
+export interface StyledListItemProps {
+  completed: boolean;
+}
+const StyledListItem = styled.span<StyledListItemProps>`
+  text-decoration: ${(props) => (props.completed ? 'line-through' : 'none')};
+`;
+
+const ListItem = ({
+  text,
+  completed,
+  completedHandler,
+  deleteHandler,
+}: ListItemProps): JSX.Element => {
+  return (
+    <>
+      <div>
+        <StyledListItem
+          completed={completed}
+          onClick={() => completedHandler()}
+        >
+          {text}
+        </StyledListItem>
+        <button onClick={() => deleteHandler()}>X</button>
+      </div>
+    </>
+  );
+};
 
 export default ListItem;
--- frontend/src/components/list/list-item/list-item.test.tsx
+++ frontend/src/components/list/list-item/list-item.test.tsx
@@ -2,10 +2,82 @@ import { render, screen } from '@testing-library/react';
 
 import ListItem from '.';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../theme';
+import userEvent from '@testing-library/user-event';
 
-test('renders ListItem text', () => {
-  const text = 'Nimble Soup';
-  render(<ListItem text={text} completed={false} index={0} />);
+const mockDeleteFn = jest.fn();
+const mockCompletedFn = jest.fn();
+
+test('renders ListItem text without strikethrough if the item is not completed', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <ListItem
+        text={'Nimble Soup'}
+        completed={false}
+        deleteHandler={mockDeleteFn}
+        completedHandler={mockCompletedFn}
+      />
+    </ThemeProvider>
+  );
   const element = screen.getByText('Nimble Soup');
+  const style = window.getComputedStyle(element);
+  expect(style.textDecoration).toBe('none');
+});
+
+test('renders ListItem text with strikethrough if the item is completed', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <ListItem
+        text={'Nimble Soup'}
+        completed={true}
+        deleteHandler={mockDeleteFn}
+        completedHandler={mockCompletedFn}
+      />
+    </ThemeProvider>
+  );
+  const element = screen.getByText('Nimble Soup');
+  const style = window.getComputedStyle(element);
+  expect(style.textDecoration).toBe('line-through');
+});
+
+test('renders ListItem text with delete button if the item is completed', () => {
+  render(
+    <ListItem
+      text={'Nimble Soup'}
+      completed={true}
+      deleteHandler={mockDeleteFn}
+      completedHandler={mockCompletedFn}
+    />
+  );
+  const element = screen.getByRole('button');
   expect(element).toBeInTheDocument();
+});
+
+test('calls deleteHandler when the delete button is clicked', () => {
+  render(
+    <ListItem
+      text={'Nimble Soup'}
+      completed={true}
+      deleteHandler={mockDeleteFn}
+      completedHandler={mockCompletedFn}
+    />
+  );
+  const element = screen.getByRole('button');
+  userEvent.click(element);
+  expect(mockDeleteFn).toBeCalledTimes(1);
+});
+
+test('calls completedHandler when the list item is clicked', () => {
+  render(
+    <ListItem
+      text={'Nimble Soup'}
+      completed={true}
+      deleteHandler={mockDeleteFn}
+      completedHandler={mockCompletedFn}
+    />
+  );
+  const element = screen.getByText('Nimble Soup');
+  userEvent.click(element);
+  expect(mockCompletedFn).toBeCalledTimes(1);
 });
